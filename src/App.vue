b840b7a85d07d30fc69e39a4fb140a6420ded5c5
Pedro Godoi <pedrogodoiriva@gmail.com>
2023-03-25 22:57:11 -0300

    Initializing Kanban

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
+import Kanban from './components/Kanban.vue'
 
 </script>
 
 <template>
-  <div class=" bg-neutral-900 ml-72 mt-1 rounded-tl-[40px] h-screen p-7">
-    
-  </div>
+  <main class=" grid grid-cols-12 text-slate-100">
+    <div class=" col-span-2">
+      <a href=""></a>
+    </div>
+    <div class="backCard bg-neutral-900 mt-[0.5vh] rounded-tl-[40px] h-[99.5vh] p-7 col-span-10">
+      <Kanban class="w-full h-full"></Kanban>
+    </div>
+  </main>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './style.css'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 
-import "primevue/resources/themes/lara-light-indigo/theme.css";     
+import "primevue/resources/themes/mdc-dark-deeppurple/theme.css";     
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 
