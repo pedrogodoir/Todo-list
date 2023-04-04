007b90a61cd745bc532c05b84d3624a265513b28
Pedro Godoi <pedrogodoiriva@gmail.com>
2023-04-04 15:01:17 -0300

    add function for delete button

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -9,19 +9,20 @@ const newTask = ref('')
 const tasks = ref([])
 
 function addTask() {
+
     if(newTask.value == null || newTask.value == '') {
         return
     }
     tasks.value.push({
-        content: newTask.value,
-        done: false,
-    })
+            content: newTask.value,
+        })
+
     newTask.value = ''
     console.log(tasks)
 }
 
-function done() {
-    tasks.value.done = !tasks.value.done
+function deleteTask(task) {
+    tasks.value = tasks.value.filter((t) => t !== task)
 }
 
 </script>
@@ -35,9 +36,14 @@ function done() {
         <div class="line bg-zinc-600 h-[2px] rounded"></div>
         <h1>Lista de Tarefas</h1>
         <div class="p-inputgroup flex-1" v-for="task in tasks">
-            <Button v-styleclass="{ selector: '@next', toggleClass: 'p-disabled' }" @click="done" label="Feito" icon="pi pi-check" iconPos="right" severity="secondary" text />
+            <Button 
+            v-styleclass="{ selector: '@next', toggleClass: 'p-disabled' }" 
+            icon="pi pi-check" 
+            iconPos="right" 
+            severity="secondary" 
+            text> </Button>
             <InputText placeholder="Nome da tarefa." v-model="task.content"></InputText>
-            <Button icon="pi pi-times" text severity="danger" />
+            <Button icon="pi pi-times" text severity="danger" @click="deleteTask(task)" />
         </div>
     </div>
 </template>
